f36698d3ac2cef5a7061996c430ee91aacd499d5
Michael G <mcg1441@uncw.edu>
2023-10-20 00:02:04 -0400

    Added Transportation fields

--- Transportation.java
+++ Transportation.java
@@ -1,2 +1,12 @@
 public class Transportation {
+    private String[] options;
+    private boolean rental;
+    private boolean ticketed;
+    private double price;
+    private int passengerSpace;
+    private double speed;
+    private String terrain;
+    private String location;
+    private String pickupTime;
+    private int travelTimeMins;
 }
