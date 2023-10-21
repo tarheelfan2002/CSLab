0ea3d351a7c805839b9c5e66528e402eb3c93832
Michael G <mcg1441@uncw.edu>
2023-10-20 20:49:07 -0400

    Added accessors and mutators

--- Transportation.java
+++ Transportation.java
@@ -9,4 +9,68 @@ public class Transportation {
     private String location;
     private String pickupTime;
     private int travelTimeMins;
+
+    public void setRental(boolean rental) {
+        this.rental = rental;
+    }
+
+    public void setTicketed(boolean ticketed) {
+        this.ticketed = ticketed;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setPassengerSpace(int passengerSpace) {
+        this.passengerSpace = passengerSpace;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setPickupTime(String pickupTime) {
+        this.pickupTime = pickupTime;
+    }
+
+    public void setTravelTimeMins(int travelTimeMins) {
+        this.travelTimeMins = travelTimeMins;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public int getPassengerSpace() {
+        return passengerSpace;
+    }
+
+    public int getTravelTimeMins() {
+        return travelTimeMins;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getPickupTime() {
+        return pickupTime;
+    }
+
+    public String getTerrain() {
+        return terrain;
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
 }
