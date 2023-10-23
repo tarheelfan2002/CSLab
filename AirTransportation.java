aadc7fbfb3bb6556c6011e10f2d67e3e2c9e5940
Michael G <mcg1441@uncw.edu>
2023-10-23 15:52:58 -0400

    Added elevation

--- AirTransportation.java
+++ AirTransportation.java
@@ -1,2 +1,11 @@
 public class AirTransportation extends Transportation {
+    private int elevation;
+
+    public void setElevation(int elevation) {
+        this.elevation = elevation;
+    }
+
+    public int getElevation() {
+        return this.elevation;
+    }
 }
